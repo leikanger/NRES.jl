96e607037b7872e926ed9354931311e5f7ccecdd
Per RL <leikange@gmail.com>
2021-07-22 16:36:09 +0200

    starta med zero-dimentional NRES

--- src/tile_map.jl
+++ src/tile_map.jl
@@ -1,4 +1,9 @@
 
 mutable struct Tile_map <: Representation
+    _dimentionality
+    _all_SAT
+    function Tile_map()
+        new(0, [Missing])
+    end
 end
 
--- test/test_tile_map.jl
+++ test/test_tile_map.jl
@@ -2,10 +2,20 @@ module TEST_TILE_MAP
 using NRES
 using Test
 
-@testset "Constructing Tile_map" begin
+@testset "Constructing Tile_map with dimensjonality ZERO" begin
     case = NRES.Tile_map()
     @test isa(case, NRES.Representation)
-    "NRES.Tile_map with empty argument"
+    " NRES.Tile_map with empty argument "
+
+    @test case._dimentionality == 0
+    " The NRES.Tile_map() empty ctor gives a zero-dimentional Tile_map (a point-representation) "
+
+    @test isa(case._all_SAT, Array)
+    " Even zero-dimensional NRES maps have an array of SAT "
+
+    @test length(case._all_SAT) == 1
+    " A zero-dimentional tile-map has exactly on SAT in this array "
+
 end
 
 end # module TEST_TILE_MAP
