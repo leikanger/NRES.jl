2e79fc31f2cbb902c41ce63e4d831220c7cd7b10
Per RL <leikange@gmail.com>
2021-07-22 22:32:43 +0200

    kan sette type igjen. Både som arg og eksplisitt ved Tile_map{TYPE}()

--- src/tile_map.jl
+++ src/tile_map.jl
@@ -2,8 +2,10 @@
 mutable struct Tile_map{T} <: Representation
     _dimentionality
     _all_SAT
-    function Tile_map{T}() where {T}
-        new(0, [T()])
+    function Tile_map(arg::T) where {T}
+        new{T}(0, [T()])
     end
 end
 
+Tile_map{T}() where {T}       = Tile_map(T()) 
+#Tile_map{T}() where {T<:Real} = Tile_map(T(0)) 
