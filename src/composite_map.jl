3baeb446a2a96cc93208f9ac24b0b32215c687a4
Per RL <leikange@gmail.com>
2021-07-29 18:31:59 +0200

    starta på composite map contructor

---
+++ src/composite_map.jl
@@ -0,0 +1,8 @@
+"""
+"""
+struct Composite_map
+    function Composite_map
+    end
+end
+
+export Composite_map
--- test/test_composite_map.jl
+++ test/test_composite_map.jl
@@ -2,5 +2,8 @@ module TEST_COMPOSITE_MAP
 using NRES
 using Test
 
+@testset "Composite map ctor" begin
+    @test_throws MethodError Composite_map()
+end
 
 end # module TEST_COMPOSITE_MAP
