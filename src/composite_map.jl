13f7affaa2dc6b09742cbe8f1f7212a29cf3f896
Per RL <leikange@gmail.com>
2021-08-02 10:26:38 +0200

    active_traits_for(composite_map) git ut array med sat som er aktive

--- src/composite_map.jl
+++ src/composite_map.jl
@@ -1,15 +1,16 @@
 """
 """
 struct Composite_map{T}
-    _nres1::T
-    _nres2::T
-    function Composite_map(nres1::T, nres2::T) where {T<:Representation}
-        new{T}(nres1, nres2)
+    _nresA::T
+    _nresB::T
+    function Composite_map(nresA::T, nresB::T) where {T<:Representation}
+        new{T}(nresA, nresB)
     end
 end
 
-function active_sat_in(nres1::Composite_map) 
-    Any[]
+function active_traits_for(map::Composite_map) 
+    # vcat(A, B) concatenates arrays "vertidally", i.e. in first dim. after removing empty entries.
+    vcat(active_traits_for(map._nresA), active_traits_for(map._nresB))
 end
 #is_active(it::Composite_map) = is_active(map_to_SAT(it._nres1)) * is_active(map_to_SAT(it._nres2))
 
--- test/test_composite_map.jl
+++ test/test_composite_map.jl
@@ -33,11 +33,11 @@ deactivate!(case)
     @test_throws MethodError Composite_map(Dummy_SAT(), false)
     " Ctor with other stuff than NRES as arg. raises MethodError "
 
-    boolean_nres1 = NRES.Tile_map{Dummy_SAT}(the_sat=Dummy_SAT())
-    boolean_nres2 = NRES.Tile_map{Dummy_SAT}(the_sat=Dummy_SAT())
-    case = Composite_map(boolean_nres1, boolean_nres2)
-    @test case._nres1 == boolean_nres1
-    @test case._nres2 == boolean_nres2
+    boolean_nresA = NRES.Tile_map{Dummy_SAT}(the_sat=Dummy_SAT())
+    boolean_nresB = NRES.Tile_map{Dummy_SAT}(the_sat=Dummy_SAT())
+    case = Composite_map(boolean_nresA, boolean_nresB)
+    @test case._nresA == boolean_nresA
+    @test case._nresB == boolean_nresB
     " Creating a composite NRES of two boolean NRES stores them as member variables "
 end
 
@@ -48,14 +48,24 @@ end
     n0_B = NRES_0(satB)
     case = Composite_map(n0_A, n0_B)
 
-    @test isempty(NRES.active_sat_in(case))
+    @test isempty(NRES.active_traits_for(case))
     " both NRES0 A and B is inactive: active sat in composite map is empty vector "
 
     activate!(satA)
-    activate!(satB)
-    #@test NRES.active_sat_in(case) == [satA, satB]
+    @test NRES.active_traits_for(case) == [satA]
+    " When satA is active: return satA "
 
-    #activate!(NRES.active_sat_in())
+    activate!(satB)
+    @test NRES.active_traits_for(case) == [satA, satB]
+    " When satA and satB is active, return both SAT "
+
+    # satC = Dummy_SAT()  # inactive by default
+    # n0_C = NRES_0(satC)
+    # case_composite2 = Composite_map(case, n0_C)
+    # @test NRES.active_traits_for(case_composite2) == [satA, satB]
+    " Before the SAT of the new NRES is active, composite-of-composites returns same as previous test "
+
+    #activate!(NRES.active_traits_for())
     # @test is_active(case) == true
 
 
